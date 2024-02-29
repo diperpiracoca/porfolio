7fbea8bc22b3140b139c62c6ebcb8ae5855ecef9
diperpiracoca <santy07piracoca@gmail.com>
2024-02-29 12:19:39 +0100

    Ir a rutas por navegacion en todos los dispositivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'porfolio';
+
+  scrollToSection(elemento:string, block :any) {
+    document.getElementById(elemento)?.scrollIntoView(
+      {
+        behavior: "smooth",
+        block: block
+      }
+    );
+  }
 }
