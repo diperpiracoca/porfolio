59d25d12088ebcc05b2c870bdd9c6027eaa8ad68
diperpiracoca <santy07piracoca@gmail.com>
2024-03-10 20:43:02 +0100

    Optimizaciones

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Proyecto} from '../../types/types'
 
 
@@ -8,23 +8,26 @@ import {Proyecto} from '../../types/types'
   styleUrls: ['./proyectos.component.scss']
 })
 
-export class ProyectosComponent  {
-   proyectos: Proyecto[]=[
-    {estado:'EN DESARROLLO', titulo:'Spot',
-    descripcion:'Red social dirigida a skaters y lugares para practicar skate',
-    imagenes:['../../assets/images/logo.svg','../../assets/images/Spot_imagen.png'],
-    RedesSociales:[{icono:'../assets/icons/github-logo.svg',nombre: 'Github', link: 'https://github.com/diperpiracoca'}]},
-    {estado:'EN DESARROLLO', titulo:'devHelper',
-    descripcion:'Aplicacion de escritorio para el desarrollo de aplicaciones.',
-    imagenes:['../../assets/images/logo.svg','../../assets/images/Spot_imagen.png'],
-    RedesSociales:[{icono:'../assets/icons/github-logo.svg',nombre: 'Github', link: 'https://github.com/diperpiracoca'}]},
-    {estado:'EN PRODUCCION', titulo:'LoopersMusic',
-    descripcion:'aplicacion movil para hacer looping',
-    imagenes:['../../assets/images/logo.svg','../../assets/images/Spot_imagen.png'],
-    RedesSociales:[{icono:'../assets/icons/github-logo.svg',nombre: 'Github', link: 'https://github.com/diperpiracoca'}]},
-    {estado:'TERMINADO', titulo:'API REST',
-    descripcion:'API que devuelve informacion de musica',
-    imagenes:['../../assets/images/logo.svg','../../assets/images/Spot_imagen.png'],
-    RedesSociales:[{icono:'../assets/icons/github-logo.svg',nombre: 'Github', link: 'https://github.com/diperpiracoca'}]}];
+export class ProyectosComponent implements OnInit {
+   ngOnInit(): void {
+    this.proyectos=[
+      {estado:'EN DESARROLLO', titulo:'Spot',
+      descripcion:'Red social dirigida a skaters y lugares para practicar skate',
+      imagenes:['../../assets/images/logo.svg','../../assets/images/Spot_imagen.png'],
+      RedesSociales:[{icono:'../assets/icons/github-logo.svg',nombre: 'Github', link: 'https://github.com/diperpiracoca'}]},
+      {estado:'EN DESARROLLO', titulo:'devHelper',
+      descripcion:'Aplicacion de escritorio para el desarrollo de aplicaciones.',
+      imagenes:['../../assets/images/logo.svg','../../assets/images/Spot_imagen.png'],
+      RedesSociales:[{icono:'../assets/icons/github-logo.svg',nombre: 'Github', link: 'https://github.com/diperpiracoca'}]},
+      {estado:'EN PRODUCCION', titulo:'LoopersMusic',
+      descripcion:'aplicacion movil para hacer looping',
+      imagenes:['../../assets/images/logo.svg','../../assets/images/Spot_imagen.png'],
+      RedesSociales:[{icono:'../assets/icons/github-logo.svg',nombre: 'Github', link: 'https://github.com/diperpiracoca'}]},
+      {estado:'TERMINADO', titulo:'API REST',
+      descripcion:'API que devuelve informacion de musica',
+      imagenes:['../../assets/images/logo.svg','../../assets/images/Spot_imagen.png'],
+      RedesSociales:[{icono:'../assets/icons/github-logo.svg',nombre: 'Github', link: 'https://github.com/diperpiracoca'}]}];
+   }
+   proyectos: Proyecto[]=[];
   
 }
